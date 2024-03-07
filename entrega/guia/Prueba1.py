ac4b71de086f7a92617b0bfd72b3e6bda8fef00e
NishXi <161514177+NishXi@users.noreply.github.com>
2024-03-07 18:42:40 +0000

    a

---
+++ entrega/guia/Prueba1.py
@@ -0,0 +1,15 @@
+print("Taller de Python P1 E2")
+#pto 1a
+print("Las máquinas me sorprenden con mucha frecuencia")
+#pto 1b
+print("")
+#pto 1c
+print("ando, caminando, con un flow violento y con un piquete cabron")
+#pto 1d
+print("23",23)
+#pto 1e
+print("Una computadora puede ser llamada \"inteligente\" si logra engañar a una persona haciéndole creer que es un humano.")
+#pto 1f
+print("Apellido Groppa","Nombre: Emanuel","edad 18")
+#pto 1g
+print("Apellido Groppa","Nombre: Emanuel","edad 18")
